eb60228806fc993dc80d728c98dd4731ba63a49b
Behemoth11 <kodohdaniel@gmail.com>
2022-07-04 20:05:22 -0500

    added initialization script

---
+++ .up.sh
@@ -0,0 +1,6 @@
+
+git clone git@github.com:awaken-behemoth/awaken-client.git &&
+mkdir awaken-servers && cd awaken-servers
+git clone git@github.com:awaken-behemoth/awaken-media-service.git
+git clone git@github.com:awaken-behemoth/awaken-email-service.git
+git clone git@github.com:awaken-behemoth/awaken-user-service.git

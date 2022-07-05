88990a5cd14affc663b88e1729c4710a766535f0
Behemoth11 <kodohdaniel@gmail.com>
2022-07-04 20:18:27 -0500

    create ressources folder and moved docker compose to ressources folder

--- .up.sh
+++ .up.sh
@@ -1,5 +1,6 @@
 
-git clone git@github.com:awaken-behemoth/awaken-client.git &&
+
+git clone git@github.com:awaken-behemoth/awaken-client.git
 mkdir awaken-servers && cd awaken-servers
 git clone git@github.com:awaken-behemoth/awaken-media-service.git
 git clone git@github.com:awaken-behemoth/awaken-email-service.git
